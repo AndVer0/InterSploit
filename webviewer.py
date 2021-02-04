3cbe2711294f4d8dcf8a29fac8a088f0bc370704
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-04 20:54:55 +0100

    Delete webviewer.py

--- webviewer.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request  import urlopen
-inputer = input("enter site(www.site.com):")
-url = 'http://' + inputer
-page = urlopen(url)
-a = BeautifulSoup(page)
-print(a.prettify())
