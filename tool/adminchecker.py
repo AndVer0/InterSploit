c09f4fb97c56161b0ccf2a7a3ce73088a5aa9b37
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-05 20:34:10 +0100

    Add files via upload

--- tool/adminchecker.py
+++ tool/adminchecker.py
@@ -1,31 +1,77 @@
-import colorama
 from colorama import Fore, Back, Style
-colorama.init()
 from urllib.request import *
 from urllib.error import *
+from prettytable import PrettyTable
 file = open('./tool/link.txt', 'r')
-link = input('Enter website name(Ex:www.site.com):')
-
-while True:
-	sub_link = file.readline().split('\n') [0]
-	
-	if not sub_link:
-		break
-	req_link = 'http://'+link+'/'+sub_link
-	
-	try:
-		reponse = urlopen(req_link)
-		
-	except HTTPError as e:
-		print(Fore.RED)
-		print('[-] No ==>', req_link)
-		continue
-		
-	except URLError as e:
-		print("[*] Url not found")
-		break
-		continue
-		
+choice = input('InSp' + Fore.RED + '(option/AP finder)' + Fore.WHITE + '>')
+myTable = PrettyTable(["Command", "Info"])
+myTable.add_row(["SUrl", "link of site (www.site.com)"])
+myTable.add_row(["run", " "])
+#show option 1
+if choice == 'show options':
+	print(myTable)
+#if choice is vide
+elif choice == '':
+	print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + choice + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
+#if choice is Surl
+elif choice == 'SUrl':
+	url = input('InSp' + Fore.RED + '(option/AP finder/SUrl)' + Fore.WHITE + '>')
+	print(Fore.RED + 'SUrl ==> ' + Fore.GREEN + url + Fore.WHITE)
+	run = input('InSp' + Fore.RED + '(option/AP finder/SUrl)' + Fore.WHITE + '>')
+#sub Surl if you choice is run(run methode)
+	if run == 'run':
+		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
+		while True:
+			link = file.readline().split('\n') [0]
+			if not link:
+				break
+			req_link = 'http://' + url + '/' + link
+			try:
+				reponse = urlopen(req_link)
+			except HTTPError as e:
+				print(Fore.RED + '[-] No ==>', req_link)
+				continue
+			except URLError as e:
+				print('[*] Url not found')
+				break
+				continue
+			else:
+				print(Fore.GREEN + '[+] Ok ==>', req_link)
+#sub Surl show option 2: indicate your url (run methode)
+	elif run == 'show options':
+		myTable1 = PrettyTable(["Command", "Info"])
+		myTable1.add_row(["SUrl", url])
+		myTable1.add_row(["run", " "])
+		print(myTable1)
+		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
+		while True:
+			link = file.readline().split('\n') [0]
+			if not link:
+				break
+			req_link = 'http://' + url + '/' + link
+			try:
+				reponse = urlopen(req_link)
+			except HTTPError as e:
+				print(Fore.RED + '[-] No ==>', req_link)
+				continue
+			except URLError as e:
+				print('[*] Url not found')
+				break
+				continue
+			else:
+				print(Fore.GREEN + '[+] Ok ==>', req_link)
+#sub Surl if you choice is vide
+	elif run == '':
+		print(myTable)
+		quit()
+#if you choice is not correct
 	else:
-		print(Fore.GREEN)
-		print("[+] Ok ==>", req_link)
+		print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + run + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
+		print(myTable)
+		quit()
+#if you choice is not correct
+else:
+	print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + choice + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
+	print(myTable)
+	quit()
+	
--- tool/webviewer.py
+++ tool/webviewer.py
@@ -1,7 +1,44 @@
 from bs4 import BeautifulSoup
+from colorama import *
 from urllib.request  import urlopen
-inputer = input("enter site(www.site.com):")
-url = 'http://' + inputer
-page = urlopen(url)
-a = BeautifulSoup(page)
-print(a.prettify())
+from prettytable import PrettyTable
+choice = input('InSp' + Fore.RED + '(option/webscarper)' + Fore.WHITE + '>')
+myTable = PrettyTable(["Command", "Info"])
+myTable.add_row(["SUrl", "link of site (www.site.com)"])
+myTable.add_row(["run", " "])
+if choice  == 'show options':
+	print(myTable)
+elif choice == 'SUrl':
+	url = input('InSp' + Fore.RED + '(option/AP finder/SUrl)' + Fore.WHITE + '>')
+	print(Fore.RED + 'SUrl ==> ' + Fore.GREEN + url + Fore.WHITE)
+	run = input('InSp' + Fore.RED + '(option/AP finder/SUrl)' + Fore.WHITE + '>')
+#sub Surl if you choice is run(run methode)
+	if run == 'run':
+		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
+		link = 'http://' + url
+		page = urlopen(link)
+		a = BeautifulSoup(page)
+		print(a.prettify())
+#run
+	elif run == 'show options':
+		myTable1 = PrettyTable(["Command", "Info"])
+		myTable1.add_row(["SUrl", url])
+		myTable1.add_row(["run", " "])
+		print(myTable1)
+		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
+		
+		link = 'http://' + url
+		page = urlopen(link)
+		a = BeautifulSoup(page)
+		print(a.prettify())
+		
+	elif run == '':
+		print(myTable)
+		quit()
+#if you choice is not correct
+	else:
+		print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + run + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
+		print(myTable)
+		quit()
+else:
+	print(Fore.RED + choice + Fore.WHITE + ' not found')
