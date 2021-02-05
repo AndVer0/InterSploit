4416522d71606c5fdac32a1aeb11b3d708ba0ea1
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-05 20:33:14 +0100

    Add files via upload

--- InSp.py
+++ InSp.py
@@ -2,17 +2,37 @@ import pyfiglet
 import os
 from colorama import Fore
 ascii_banner = pyfiglet.figlet_format("InterSploit")
-print(Fore.BLUE)
 print(ascii_banner)
-print(Fore.WHITE)
-print('1: admin panel finder (bruteforcer)')
-print('2: web scarper(extract data)')
-print('  ')
-check = input("enter number:")
-if check == '1':
-		os.system('python3 ./tool/adminchecker.py')
-elif check == '2':
-    	os.system('python3 ./tool/webviewer.py')
+print('use ' + Fore.RED + 'show options ' + Fore.WHITE + 'to show options')
+show = input('InSp>')
+if show == "show options":
+	print(Fore.LIGHTBLUE_EX + '1. admin panel finder ' + Fore.GREEN + '(bruteforcer)' + "\n" + Fore.LIGHTBLUE_EX + '2. web scarper ' + Fore.GREEN + '(extract data)' + Fore.WHITE)
 else:
-	print('number not found')
+	print(show + Fore.RED +' not found' + Fore.WHITE)
+	print(Fore.GREEN + '[*]' + Fore.WHITE + 'use ' + Fore.RED + 'show options' + Fore.WHITE)
+	quit()
+choice = input('InSp' + Fore.RED + '(choice number)' + Fore.WHITE + '>')
+if choice == '1':
+	os.system('python ./tool/adminchecker.py')
+elif choice == '2':
+	os.system('python ./tool/webviewer.py')
+else:
+	print(Fore.RED + 'Error number' + Fore.WHITE)
+	print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + choice + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
+	quit()
 	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
