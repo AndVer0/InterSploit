667f126312f80c578acd58bb2df6d4d9b9065ecc
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-06 14:07:28 +0100

    Add files via upload

--- InSp.py
+++ InSp.py
@@ -7,6 +7,7 @@ print('use ' + Fore.RED + 'show options ' + Fore.WHITE + 'to show options')
 show = input('InSp>')
 if show == "show options":
 	print(Fore.LIGHTBLUE_EX + '1. admin panel finder ' + Fore.GREEN + '(bruteforcer)' + "\n" + Fore.LIGHTBLUE_EX + '2. web scarper ' + Fore.GREEN + '(extract data)' + Fore.WHITE)
+	print(Fore.LIGHTBLUE_EX + '3. Port Scanner ' + Fore.GREEN + '(Fast Bruteforcer)' + Fore.WHITE)
 else:
 	print(show + Fore.RED +' not found' + Fore.WHITE)
 	print(Fore.GREEN + '[*]' + Fore.WHITE + 'use ' + Fore.RED + 'show options' + Fore.WHITE)
@@ -16,6 +17,8 @@ if choice == '1':
 	os.system('python ./tool/adminchecker.py')
 elif choice == '2':
 	os.system('python ./tool/webviewer.py')
+elif choice == '3':
+	os.system('python ./tool/portscanner.py')
 else:
 	print(Fore.RED + 'Error number' + Fore.WHITE)
 	print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + choice + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
