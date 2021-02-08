19aaafead05c4a43083169f40c9df9214974b5cf
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-08 13:41:48 +0100

    Add files via upload

--- InSp.py
+++ InSp.py
@@ -2,22 +2,24 @@ import pyfiglet
 import os
 from colorama import *
 ascii_banner = pyfiglet.figlet_format("InterSploit")
-print(ascii_banner + '                                                 ' + Fore.LIGHTCYAN_EX + 'v 0.1.2' + Fore.WHITE)
+print(ascii_banner + '                                                 ' + Fore.LIGHTCYAN_EX + 'v 0.1.3' + Fore.WHITE)
 print('use ' + Fore.RED + 'show options ' + Fore.WHITE + 'to show options')
 show = input('InSp>')
 if show == "show options":
-	print(Fore.LIGHTBLUE_EX + '1. admin panel finder ' + Fore.GREEN + '(bruteforcer)' + "\n" + Fore.LIGHTBLUE_EX + '2. web scarper ' + Fore.GREEN + '(extract data)' + Fore.WHITE)
-	print(Fore.LIGHTBLUE_EX + '3. Port Scanner ' + Fore.GREEN + '(Fast Bruteforcer)' + Fore.WHITE)
+	print('   option/AP finder')
+	print('   option/webscarper')
+	print('   option/portscanner')
 else:
 	print(show + Fore.RED +' not found' + Fore.WHITE)
 	print(Fore.GREEN + '[*]' + Fore.WHITE + 'use ' + Fore.RED + 'show options' + Fore.WHITE)
 	quit()
 choice = input('InSp' + Fore.RED + '(choice number)' + Fore.WHITE + '>')
-if choice == '1':
+
+if choice == 'use option/AP finder':
 	os.system('python ./tool/adminchecker.py')
-elif choice == '2':
+elif choice == 'use option/webscarper':
 	os.system('python ./tool/webviewer.py')
-elif choice == '3':
+elif choice == 'use option/portscanner':
 	os.system('python ./tool/portscanner.py')
 else:
 	print(Fore.RED + 'Error number' + Fore.WHITE)
