b4d5b9fd4cf87c1810dcbfc49ba77f4b86033c39
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-16 13:36:20 +0100

    Add files via upload

--- tool/xss.py
+++ tool/xss.py
@@ -1,7 +1,107 @@
 from colorama import *
-import sys
+from urllib.parse import urljoin
+from pprint import pprint
+from bs4 import BeautifulSoup as bs
 import requests
 from prettytable import PrettyTable
+
+def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
+    
+def get_form_details(form):
+    """
+    This function extracts all possible useful information about an HTML `form`
+    """
+    details = {}
+    # get the form action (target url)
+    action = form.attrs.get("action").lower()
+    # get the form method (POST, GET, etc.)
+    method = form.attrs.get("method", "get").lower()
+    # get all the input details such as type and name
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        inputs.append({"type": input_type, "name": input_name})
+    # put everything to the resulting dictionary
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+    """
+    This function extracts all possible useful information about an HTML `form`
+    """
+    details = {}
+    # get the form action (target url)
+    action = form.attrs.get("action").lower()
+    # get the form method (POST, GET, etc.)
+    method = form.attrs.get("method", "get").lower()
+    # get all the input details such as type and name
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        inputs.append({"type": input_type, "name": input_name})
+    # put everything to the resulting dictionary
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+def submit_form(form_details, url, value):
+    """
+    Submits a form given in `form_details`
+    Params:
+        form_details (list): a dictionary that contain form information
+        url (str): the original URL that contain that form
+        value (str): this will be replaced to all text and search inputs
+    Returns the HTTP Response after form submission
+    """
+    # construct the full URL (if the url provided in action is relative)
+    target_url = urljoin(url, form_details["action"])
+    # get the inputs
+    inputs = form_details["inputs"]
+    data = {}
+    for input in inputs:
+        # replace all text and search values with `value`
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            # if input name and value are not None, 
+            # then add them to the data of form submission
+            data[input_name] = input_value
+
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        # GET request
+        return requests.get(target_url, params=data)
+def scan_xss(url):
+    """
+    Given a `url`, it prints all XSS vulnerable forms and 
+    returns True if any is vulnerable, False otherwise
+    """
+    # get all the forms from the URL
+    forms = get_all_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+    js_script = "<Script>alert('hi')</scripT>"
+    # returning value
+    is_vulnerable = False
+    # iterate over all forms
+    for form in forms:
+        form_details = get_form_details(form)
+        content = submit_form(form_details, url, js_script).content.decode()
+        if js_script in content:
+            print(f"[+] XSS Detected on {url}")
+            print(f"[*] Form details:")
+            pprint(form_details)
+            is_vulnerable = True
+            # won't break because we want to print available vulnerable forms
+    return is_vulnerable
+    
 choice = input('InSp' + Fore.RED + '(option/XSS scanner)' + Fore.WHITE + '>')
 myTable = PrettyTable(["Command", "Info"])
 myTable.add_row(["SUrl", "host (ex: www.example.com + get_name (ex: ?item= ))"])
@@ -10,28 +110,22 @@ if choice  == 'show options':
 	print(myTable)
 	quit()
 elif choice == 'SUrl':
-	url = input('InSp' + Fore.RED + '(option/XSS scanner/SUrl)' + Fore.WHITE + '>')
-	print(Fore.RED + 'SUrl ==> ' + Fore.GREEN + url + Fore.WHITE)
+	url1 = input('InSp' + Fore.RED + '(option/XSS scanner/SUrl)' + Fore.WHITE + '>')
+	print(Fore.RED + 'SUrl ==> ' + Fore.GREEN + url1 + Fore.WHITE)
 	run = input('InSp' + Fore.RED + '(option/XSS scanner/SUrl)' + Fore.WHITE + '>')
 	if run == 'run':
 		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
-		payload = "<script>alert('XSS');</script>"
-		req = requests.get(url + payload, "html.parser").text
-		if payload in req:
-			print('XSS vulnerability ' + Fore.GREEN + 'found')
-		else:
-			print('XSS vulnerability ' + Fore.RED + 'not found')
+		if __name__ == "__main__":
+			url = url1
+			print(scan_xss(url))
 	elif run == 'show options':
 		myTable1 = PrettyTable(["Command", "Info"])
-		myTable1.add_row(["SUrl", url])
+		myTable1.add_row(["SUrl", url1])
 		myTable1.add_row(["run", " "])
 		print(myTable1)
 		print('[' + Fore.BLUE + '*' + Fore.WHITE + ']' + 'starting...')
-		payload = "<script>alert('XSS');</script>"
-		req = requests.get(url + payload, "html.parser").text
-		if payload in req:
-			print('XSS vulnerability ' + Fore.GREEN + 'found')
-		else:
-			print('XSS vulnerability ' + Fore.RED + 'not found')
+		if __name__ == "__main__":
+			url = url1
+			print(scan_xss(url))		
 else:
 	print(Fore.RED + choice + Fore.WHITE + 'not found')
