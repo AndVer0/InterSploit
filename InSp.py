cfc717489800af09e8132895f5a0dd2a804920a9
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2021-02-21 00:50:25 +0100

    Add files via upload

--- InSp.py
+++ InSp.py
@@ -1,8 +1,8 @@
-import pyfiglet
-import os
+import pyfiglet, os 
 from colorama import *
+os.system('clear')
 ascii_banner = pyfiglet.figlet_format("InterSploit")
-print(ascii_banner + '                                                 ' + Fore.LIGHTCYAN_EX + 'v 3.1.5' + Fore.WHITE)
+print(ascii_banner + '                                                 ' + Fore.LIGHTCYAN_EX + 'v 3.1.6' + Fore.WHITE)
 print('use ' + Fore.RED + 'show options ' + Fore.WHITE + 'to show options')
 print('use ' + Fore.RED + 'use option/ ' + Fore.WHITE + 'to use option')
 show = input('InSp>')
@@ -13,13 +13,13 @@ if show == "show options":
 	print('   option/AR finder')
 	print('   option/SD detector')
 	print('   option/XSS scanner')
-	print('   option/geoip')
 	print('   option/SQLiScanner')
 else:
 	print(show + Fore.RED +' not found' + Fore.WHITE)
 	print(Fore.GREEN + '[*]' + Fore.WHITE + 'use ' + Fore.RED + 'show options' + Fore.WHITE)
 	print(Fore.GREEN + '[*]' + Fore.WHITE + 'use ' + Fore.RED + 'use option/' + Fore.WHITE)
-	quit()
+	os.system('clear')
+	os.system('python InSp.py')
 choice = input('InSp' + Fore.RED + '(choice one)' + Fore.WHITE + '>')
 
 if choice == 'use option/AP finder':
@@ -34,14 +34,13 @@ elif choice == 'use option/SD detector':
 	os.system('python ./tool/sdd.py')
 elif choice == 'use option/XSS scanner':
 	os.system('python ./tool/xss.py')
-elif choice == 'use option/geoip':
-	os.system('python ./tool/geoip.py')
 elif choice == 'use option/SQLiScanner':
 	os.system('python ./tool/sqlmap.py')
 else:
 	print(Fore.RED + 'Error number' + Fore.WHITE)
 	print(Fore.GREEN + '[!]' + Fore.WHITE + '[ ' + choice + ' ]' + Fore.RED + ' not found' + Fore.WHITE)
-	quit()
+	os.system('clear')
+	os.system('python InSp.py')
 	
 
 	
